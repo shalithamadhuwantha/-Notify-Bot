ac888bcf5882adce2dcf82e89323707ff0304d55
Shalitha Madhuwantha <78067281+shalithamadhuwantha@users.noreply.github.com>
2023-11-23 23:07:38 +0530

    Update setting.js

--- setting/setting.js
+++ setting/setting.js
@@ -1,1 +1,18 @@
-le
+[
+    "wabot",
+    "webernetco",
+    "whatsapp",
+    "whatsapp bot",
+    "time reminder bot",
+    "school management",
+    "lecture time",
+    "alert bot",
+    "notify bot",
+    "notify whatsapp bot",
+    "weber",
+    "webernetco",
+    "shalitha madhuwantha"
+  ],
+  "author": "ShAlItHa",
+  "license": "ISC",
+  "description": "This bot is a time reminder bot ... I will help you to manage your lecture or other class schedule .. this will definitely deliver the lecture or class time on time"
