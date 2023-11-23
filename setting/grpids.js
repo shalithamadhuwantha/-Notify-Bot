848eb851d3b068b2e1c11e61eb70e2028838c654
Shalitha Madhuwantha <78067281+shalithamadhuwantha@users.noreply.github.com>
2023-11-23 23:04:52 +0530

    Update grpids.js

--- setting/grpids.js
+++ setting/grpids.js
@@ -1,7 +1,7 @@
 // ***** DONT REMOVE THIS FILE
 const admin_profile_id = ""; 
-const admin_profile_no = "94740082154"; 
-const control_grp_id = "120363179842403376@g.us"; 
+const admin_profile_no = "94788743616"; 
+const control_grp_id = "0123785@g.us"; 
 
 module.exports={
     admin_profile_no,
